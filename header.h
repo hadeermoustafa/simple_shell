c48ab129a4e5dac7da904fbbbc2d64492f5a651c
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-26 16:06:13 -0700

    add header

---
+++ header.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
